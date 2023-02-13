de3dee56584db3100af611b740d382f3223f0603
Alex Pitzel <pitzelalex@gmail.com>
2023-02-13 12:50:20 -0500

    create: DB and password field and basic registration form"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User <ApplicationRecord 
     validates_presence_of :email, :name 
     validates_uniqueness_of :email
+    validates_presence_of :password
 
+    has_secure_password
 end 
--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -5,24 +5,27 @@ RSpec.describe "User Registration" do
     visit register_path
 
     fill_in :user_name, with: 'User One'
-    fill_in :user_email, with:'user1@example.com'
+    fill_in :user_email, with: 'user1@example.com'
+    fill_in :user_password, with: 'testpassword'
+    fill_in :user_password_confirmation, with: 'testpassword'
     click_button 'Create New User'
 
     expect(current_path).to eq(user_path(User.last.id))
     expect(page).to have_content("User One's Dashboard")
-  end 
+  end
 
   it 'does not create a user if email isnt unique' do 
     User.create(name: 'User One', email: 'notunique@example.com')
 
     visit register_path
-    
+
     fill_in :user_name, with: 'User Two'
-    fill_in :user_email, with:'notunique@example.com'
+    fill_in :user_email, with: 'notunique@example.com'
+    fill_in :user_password, with: 'testpassword'
+    fill_in :user_password_confirmation, with: 'testpassword'
     click_button 'Create New User'
 
     expect(current_path).to eq(register_path)
     expect(page).to have_content("Email has already been taken")
-    
   end
 end
