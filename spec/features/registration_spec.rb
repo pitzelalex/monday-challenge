5a439c15b0471fa6a4fbde71e62b6f90c043b437
Alex Pitzel <pitzelalex@gmail.com>
2023-02-13 13:29:05 -0500

    test: Create sad path tests for password and name for registering a new user

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "User Registration" do
     expect(page).to have_content("User One's Dashboard")
   end
 
-  it 'does not create a user if email isnt unique' do 
+  it 'does not create a user if email isnt unique' do
     User.create(name: 'User One', email: 'notunique@example.com', password: 'testpassword')
 
     visit register_path
@@ -26,6 +26,39 @@ RSpec.describe "User Registration" do
     click_button 'Create New User'
 
     expect(current_path).to eq(register_path)
-    expect(page).to have_content("Email has already been taken")
+    expect(page).to have_content('Email has already been taken')
+  end
+
+  it 'does not create a user if name is missing' do
+    visit register_path
+
+    fill_in :user_email, with: 'user1@example.com'
+    fill_in :user_password, with: 'testpassword'
+    fill_in :user_password_confirmation, with: 'testpassword'
+    click_button 'Create New User'
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Name can't be blank")
+  end
+
+  it 'does not create a user if passwords do not match' do
+    visit register_path
+
+    fill_in :user_name, with: 'User One'
+    fill_in :user_email, with: 'user1@example.com'
+    fill_in :user_password, with: 'testpassword'
+    fill_in :user_password_confirmation, with: 'differentpassword'
+    click_button 'Create New User'
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password confirmation doesn't match Password")
+
+    fill_in :user_name, with: 'User One'
+    fill_in :user_email, with: 'user1@example.com'
+    fill_in :user_password, with: 'testpassword'
+    click_button 'Create New User'
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password confirmation doesn't match Password")
   end
 end
